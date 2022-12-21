1d99dc90c95c2f81be6f36a3476fdecff01d82ea
Evgeniy Sterekhov <john2331832@gmail.com>
2022-12-21 14:03:17 +0500

    Create Transition

--- app/src/main/java/ru/netoloy/statsview/MainActivity.kt
+++ app/src/main/java/ru/netoloy/statsview/MainActivity.kt
@@ -2,6 +2,10 @@ package ru.netoloy.statsview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
+import android.view.ViewGroup
+import android.transition.Scene
 import ru.netoloy.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
@@ -9,14 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val statsView = findViewById<StatsView>(R.id.statsView)
-        val data = listOf(
-            500F,
-            500F,
-            500F,
-            500F,
-        )
-        statsView.data = data.map {
-            it / data.sum()
+        val root = findViewById<ViewGroup>(R.id.root)
+        val scene = Scene.getSceneForLayout(root, R.layout.end_scane, this)
+        findViewById<View>(R.id.goButton).setOnClickListener {
+           TransitionManager.go(scene)
         }
     }
 }
